b122126411ae92a2adf4848ff54977b7b278a562
Mariam Farghali <mariamfarghaly930@gmail.com>
2025-08-10 16:41:20 +0300

    Your commit message here

--- HrRagApi/Models/User.cs
+++ HrRagApi/Models/User.cs
@@ -4,11 +4,9 @@ namespace HrRagApi.Models;
 
 public class User
 {
-    public Guid UserId { get; set; } = Guid.NewGuid();
-    //public virtual ICollection<Query> Queries { get; set; } = new List<Query>();
+    public Guid UserId { get; set; } = Guid.NewGuid(); //version 4 -> intialized randomly
     public ICollection<Query> Queries { get; set; } = new List<Query>();
     
-
     [Required]
     [MaxLength(50)]
     public string Username { get; set; } = null!;
